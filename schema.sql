dba05df0bb03c048d8eb188a1f31d9ec03d5209e
laurenalyn <laurenalynwilkerson@gmail.com>
2019-01-06 13:53:42 -0500

    changes order of data entries

--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE users (
     firstname TEXT,
     lastname TEXT,
+    phone_number TEXT,
+    email TEXT,
     username VARCHAR,
     user_password VARCHAR,
     wifi_network VARCHAR,
-    wifi_password VARCHAR,
-    phone_number TEXT,
-    email TEXT
+    wifi_password VARCHAR
 );
--- seed.sql
+++ seed.sql
@@ -1,15 +1,14 @@
-INSERT INTO users (firstname, lastname, username, user_password, wifi_network, wifi_password, phone_number, email) 
-VALUES ('April', 'Copes', 'empathy91', 'lingo2555', 'wifi_network123', 'wifipass123', '1234567891', 'antdevelopment1@gmail.com');
-INSERT INTO users (firstname, lastname, username, user_password, wifi_network, wifi_password, phone_number, email) 
-VALUES ('Lauren', 'Wilkerson', 'lillylove123', 'hillpassword456', 'wifinetwork444', 'wifipassword999', '3334443456', 'laurenalyn@gmail.com');
-INSERT INTO users (firstname, lastname, username, user_password, wifi_network, wifi_password, phone_number, email) 
-VALUES ('Chris', 'Cruz', 'userisnamed2', 'password9955', 'wifinet9494', 'wifipass030', '3980380393', 'killergreen200@yahoo.com');
-INSERT INTO users (firstname, lastname, username, user_password, wifi_network, wifi_password, phone_number, email) 
-VALUES ('Beverly', 'Hill', 'user999', 'userpasss000', 'wifinetgreen99393', 'wifipassword39393', '8287475656', 'yellowgreen@aol.com');
-INSERT INTO users (firstname, lastname, username, user_password, wifi_network, wifi_password, phone_number, email) 
-VALUES ('Shannon', 'Hill', 'username9494', 'userpass9373', 'wifinet9375', 'wifipass9428', '9374892837', 'justinwing@gmail.com');
-INSERT INTO users (firstname, lastname, username, user_password, wifi_network, wifi_password, phone_number, email) 
-VALUES ('Wilson', 'Crow', 'username03932', 'userpass8273834', 'wifinet333333', 'wifipass039404', '9283746578', 'justinwho@hotmail.com');
-INSERT INTO users (firstname, lastname, username, user_password, wifi_network, wifi_password, phone_number, email) 
-VALUES ('Dawson', 'Creek', 'usercastedname222', 'userpass03003', 'wifinet0382393', 'wifipass0920202', '8387476574', 'greenleaves@gmail.com');
-
+INSERT INTO users (firstname, lastname, phone_number, email, username, user_password, wifi_network, wifi_password) 
+VALUES ('April', 'Copes', '1234567891', 'antdevelopment1@gmail.com', 'empathy91', 'lingo2555', 'wifi_network123', 'wifipass123');
+INSERT INTO users (firstname, lastname, username, phone_number, email, user_password, wifi_network, wifi_password) 
+VALUES ('Lauren', 'Wilkerson', '3334443456', 'laurenalyn@gmail.com', 'lillylove123', 'hillpassword456', 'wifinetwork444', 'wifipassword999');
+INSERT INTO users (firstname, lastname, phone_number, email, username, user_password, wifi_network, wifi_password) 
+VALUES ('Chris', 'Cruz', '3980380393', 'killergreen200@yahoo.com', 'userisnamed2', 'password9955', 'wifinet9494', 'wifipass030');
+INSERT INTO users (firstname, lastname, phone_number, email, username, user_password, wifi_network, wifi_password) 
+VALUES ('Beverly', 'Hill','8287475656', 'yellowgreen@aol.com', 'user999', 'userpasss000', 'wifinetgreen99393', 'wifipassword39393');
+INSERT INTO users (firstname, lastname, phone_number, email, username, user_password, wifi_network, wifi_password) 
+VALUES ('Shannon', 'Hill','9374892837', 'justinwing@gmail.com', 'username9494', 'userpass9373', 'wifinet9375', 'wifipass9428');
+INSERT INTO users (firstname, lastname, phone_number, email, username, user_password, wifi_network, wifi_password) 
+VALUES ('Wilson', 'Crow', '9283746578', 'justinwho@hotmail.com', 'username03932', 'userpass8273834', 'wifinet333333', 'wifipass039404');
+INSERT INTO users (firstname, lastname, phone_number, email, username, user_password, wifi_network, wifi_password) 
+VALUES ('Dawson', 'Creek', '8387476574', 'greenleaves@gmail.com', 'usercastedname222', 'userpass03003', 'wifinet0382393', 'wifipass0920202');
