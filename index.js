42c9de2453929daf7279eb4851afdab4fc5a0bf5
April Copes <antdevelopment1@gmail.com>
2019-01-08 16:19:19 -0500

    adds create functions

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_TOKEN);
-const User = require('./models/User');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const User = require('./models/User');
+const Product = require('./models/Product');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -34,5 +35,67 @@ app.post('/intruder', (req, res) => {
 app.listen(3000, () => {
     console.log('Listening on port 3000');
 })
+
+
+
+    // =====================
+    //      Create User
+    // =====================
+
+    // Create a new user
+    // User.createUser("Justin", "Tenson", "crewman@123.com", "username123", "userpassword333")
+    //     .then(newUser => {
+    //         console.log(newUser);
+    //     })
+
+    // =====================
+    //     Retrieve User
+    // =====================
+
+    
+
+
+
+
+    // ================================
+    //     Create / Register Product
+    // ================================
+
+    // Create/Register new product
+
+    // =============================
+    //     Retrieve User Product
+    // =============================
+
+    // Retrieve all products
+
+    // Retrieve product by id
+
+    // Retrieve product by serialNumber
+
+    // Retrieve phone number
+
+    // Retrieve user by product id
+    
+    // =============================
+    //      Update User Product
+    // =============================
+
+    // Update serial number
+
+    // Update phone number
+
+
+    // =============================
+    //      Delete User Product
+    // =============================
+
+    // Delete product by id
+
+    // Delete product by serial number
+
+
+
+
 
 
