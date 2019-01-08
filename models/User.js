71373097b20e31d68385b953228689a016c2fdc6
April Copes <antdevelopment1@gmail.com>
2019-01-08 16:16:17 -0500

    Adds create function

--- models/User.js
+++ models/User.js
@@ -2,39 +2,37 @@ const db = require('./db');
 
 class User {
 
-    constructor (
-        firstName, lastName, userName, userPassword, wifiNetwork, wifiPassword, phoneNumber, email
-    ) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.userName = userName;
-        this.userPassword = userPassword;
-        this.wifiNetwork = wifiNetwork;
-        this.wifiPassword = wifiPassword;
-        this.phoneNumber = phoneNumber;
+    constructor (id, firstname, lastname, email, username, user_password) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
         this.email = email;
+        this.username = username;
+        this.user_password = user_password;
     }
 
-    // Test
-    static hello () {
-        console.log('hello there');
-    }
-
-    // =====================
-    //        Create
-    // =====================
-
-    // =====================
-    //       Retrieve
-    // =====================
     
     // =====================
-    //        Update
+    //      Create User
     // =====================
 
+    // Create a new user
+    static createUser(firstname, lastname, email, username, user_password) {
+        return db.one(`
+        insert into users (firstname, lastname, email, username, user_password) 
+        values($1, $2, $3, $4, $5) returning id, firstname, lastname, email, username, user_password`, 
+        [firstname, lastname, email, username, user_password])
+            .then(result => {
+                const newUser = new User(result.id, firstname, lastname, email, username, user_password);
+                return newUser;
+            })
+    }
+
+
     // =====================
-    //        Delete
+    //     Retrieve User
     // =====================
+
 }
 
 module.exports = User;
