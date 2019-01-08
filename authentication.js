685e005727949d9040e6d93b69ede51ececa5f52
April Copes <antdevelopment1@gmail.com>
2019-01-08 16:12:03 -0500

    Adds inital views files

--- authentication.js
+++
@@ -1,7 +0,0 @@
-const authentication = pgp({
-    SID: process.env.SID,
-    AUTH_TOKEN: process.env.AUTH_TOKEN
-});
-// ============================================
-
-module.exports = authentication;
