cfb73232715c047b7a573fa5b4ad6637463f5ad8
April Copes <antdevelopment1@gmail.com>
2019-01-08 16:13:45 -0500

    uodated database structure

---
+++ schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    firstname TEXT,
+    lastname TEXT,
+    email TEXT,
+    username VARCHAR,
+    user_password VARCHAR
+);
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    phone_number TEXT,
+    user_id INTEGER REFERENCES users (id) ON DELETE CASCADE
+);
