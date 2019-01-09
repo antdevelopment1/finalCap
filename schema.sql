401db7d531c918aeb2ab1a85e54077324ae9cdf8
April Copes <antdevelopment1@gmail.com>
2019-01-09 17:15:21 -0500

    adds serial number to databass

--- schema.sql
+++ schema.sql
@@ -9,6 +9,7 @@ CREATE TABLE users (
 
 CREATE TABLE products (
     id SERIAL PRIMARY KEY,
+    serial_number TEXT,
     phone_number TEXT,
     user_id INTEGER REFERENCES users (id) ON DELETE CASCADE
 );
--- seed.sql
+++ seed.sql
@@ -14,17 +14,17 @@ INSERT INTO users (firstname, lastname, email, username, user_password)
 VALUES ('Dawson', 'Creek', 'greenleaves@gmail.com', 'usercastedname222', 'userpass03003');
 
 
-INSERT INTO products(phone_number, user_id) 
-VALUES ('9393938473', 1);
-INSERT INTO products(phone_number, user_id) 
-VALUES ('30303837636', 2);
-INSERT INTO products(phone_number, user_id) 
-VALUES ('2094857283', 3);
-INSERT INTO products(phone_number, user_id) 
-VALUES ('0987653456', 4);
-INSERT INTO products(phone_number, user_id) 
-VALUES ('3937482983', 5);
-INSERT INTO products(phone_number, user_id) 
-VALUES ('3563783647', 6);
-INSERT INTO products(phone_number, user_id) 
-VALUES ('0987896789', 7);
+INSERT INTO products(serial_number, phone_number, user_id) 
+VALUES ('8282828920-2', '9393938473', 1);
+INSERT INTO products(serial_number, phone_number, user_id) 
+VALUES ('827820022', '30303837636', 2);
+INSERT INTO products(serial_number, phone_number, user_id) 
+VALUES ('9829992022', '2094857283', 3);
+INSERT INTO products(serial_number, phone_number, user_id) 
+VALUES ('98299202002', '0987653456', 4);
+INSERT INTO products(serial_number, phone_number, user_id) 
+VALUES ('82888288292', '3937482983', 5);
+INSERT INTO products(serial_number, phone_number, user_id) 
+VALUES ('2828288282', '3563783647', 6);
+INSERT INTO products(serial_number, phone_number, user_id) 
+VALUES ('929292992', '0987896789', 7);
