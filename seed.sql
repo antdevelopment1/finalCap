d659a350a1692798f59feb227fd178f58a382a10
April Copes <antdevelopment1@gmail.com>
2019-01-09 19:08:01 -0500

    accepts changes

--- seed.sql
+++ seed.sql
@@ -14,22 +14,6 @@ INSERT INTO users (firstname, lastname, email, username, user_password)
 VALUES ('Dawson', 'Creek', 'greenleaves@gmail.com', 'usercastedname222', 'userpass03003');
 
 
-<<<<<<< HEAD
-INSERT INTO product (product_number, wifi_network, wifi_password, phone_number, user_id)
-VALUES ('010101', 'wifinet01', 'wifipass01', '1234567890', 1);
-INSERT INTO product (product_number, wifi_network, wifi_password, phone_number, user_id)
-VALUES ('020202', 'wifinet02', 'wifipass02', '0987654321', 2);
-INSERT INTO product (product_number, wifi_network, wifi_password, phone_number, user_id)
-VALUES ('030303', 'wifinet03', 'wifipass03', '1029384756', 3);
-INSERT INTO product (product_number, wifi_network, wifi_password, phone_number, user_id)
-VALUES ('040404', 'wifinet04', 'wifipass04', '0192837465', 4);
-INSERT INTO product (product_number, wifi_network, wifi_password, phone_number, user_id)
-VALUES ('050505', 'wifinet05', 'wifipass05', '6574839201', 5);
-INSERT INTO product (product_number, wifi_network, wifi_password, phone_number, user_id)
-VALUES ('060606', 'wifinet06', 'wifipass06', '5647382910', 6);
-INSERT INTO product (product_number, wifi_network, wifi_password, phone_number, user_id)
-VALUES ('070707', 'wifinet07', 'wifipass07', '6789054321', 7);
-=======
 INSERT INTO products(serial_number, phone_number, user_id) 
 VALUES ('8282828920-2', '9393938473', 1);
 INSERT INTO products(serial_number, phone_number, user_id) 
@@ -44,4 +28,3 @@ INSERT INTO products(serial_number, phone_number, user_id)
 VALUES ('2828288282', '3563783647', 6);
 INSERT INTO products(serial_number, phone_number, user_id) 
 VALUES ('929292992', '0987896789', 7);
->>>>>>> 29210e1ac56a5c4a508a2bfccb44db45d59e83bb
